ccc0cd3c970ec1b8d50c44f6c9134578cafe650c
Rendy-ptr <rendyp348@gmail.com>
2025-11-19 12:31:48 +0700

    feat(CompositionComponent.vue): add dynamic class, id, and button with loading state

--- src/components/CompositionComponent.vue
+++ src/components/CompositionComponent.vue
@@ -2,9 +2,28 @@
 const name = 'This A Composition Component'
 
 const showName = () => console.log(name)
+
+const id = '1'
+const titleClass = 'text-2xl font-bold'
+
+const isLoading = true
+
+const styleButton = {
+  disabled: isLoading
+}
+
+const getLoadingMessage = () => {
+  if (isLoading) {
+    return 'Loading....'
+  } else {
+    return 'Submit'
+  }
+}
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
+  <h1 :id="id" :class="titleClass">{{ name }}</h1>
   <button @click="showName">Composition Component</button>
+  <br></br>
+  <button v-bind="styleButton">{{ getLoadingMessage() }}</button>
 </template>
