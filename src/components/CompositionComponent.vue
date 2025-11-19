905b8928befd6183133b98ee44850803841f04ed
Rendy-ptr <rendyp348@gmail.com>
2025-11-19 11:39:31 +0700

    feat: replace default starter components with custom components

---
+++ src/components/CompositionComponent.vue
@@ -0,0 +1,10 @@
+<script setup>
+const name = 'This A Composition Component'
+
+const showName = () => console.log(name)
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <button @click="showName">Composition Component</button>
+</template>
---
+++ src/components/OptionsComponent.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'This A Options Component',
+    }
+  },
+  methods: {
+    showName() {
+      console.log(this.name)
+    },
+  },
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <button @click="showName">Options Component</button>
+</template>
